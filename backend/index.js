d2ffc64b8b4de9165dc690d2d39320af33ec506f
SuzySux <suzettenava@hotmail.com>
2026-02-10 00:47:04 -0600

    yupi

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import {createServer} from 'node:http';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = process.env.PORT || 3000;
+const app = express();
+const server = createServer(app);
+app.use('/', express.static(path.join(__dirname, '../frontend')));
+//await significa que js debe esperar a que la linea termine antes de ejecutar la siguiente.
+await server.listen (port);
+console.log(`Server is running on port ${port}`);
