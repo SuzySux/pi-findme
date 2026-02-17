63932981ab4f65ee48b2c2a023c70ecfe0bda19f
SuzySux <suzettenava@hotmail.com>
2026-02-17 09:43:08 -0600

    ahi vamos

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,13 @@ const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 3000;
 const app = express();
 const server = createServer(app);
-app.use('/', express.static(path.join(__dirname, '../frontend')));
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.use('/public', express.static(path.join(__dirname, '../frontend')));
+app.get('/', (req, res) => {
+    res.render('index');
+});
 //await significa que js debe esperar a que la linea termine antes de ejecutar la siguiente.
 await server.listen (port);
 console.log(`Server is running on port ${port}`);
+//fua wtf
